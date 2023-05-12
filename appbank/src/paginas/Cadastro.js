41b93cf500f116a9e5983278248efc90737b1c5e
Bia Claudino <105952054+BeatrizClaudino@users.noreply.github.com>
2023-05-12 16:54:42 -0300

    appbank
    quase terminando de criar o usuário

--- appbank/src/paginas/Cadastro.js
+++ appbank/src/paginas/Cadastro.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, Button } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, Button, Alert } from 'react-native';
 import Botao from '../componentes/Button';
 
 export default function Cadastro({ navigation }) {
@@ -8,52 +8,84 @@ export default function Cadastro({ navigation }) {
   const [email, setEmail] = useState('');
   const [datanascimento, setDatanascimento] = useState('');
   const [telefone, setTelefone] = useState('');
+  const [senha, setSenha] = useState('');
+  const [confirmarSenha, setConfirmarSenha] = useState('');
   const [passo, setPasso] = useState(1);
-  //PARA REALIZAR O UPLOAD TODAS AS CONDIÇÕES DEVEM SER ATENDIDAS
-  const upload = (e) => {
-    e.preventDefault()
 
+  console.log(senha)
+  console.log(confirmarSenha)
+
+  //PARA REALIZAR O UPLOAD TODAS AS CONDIÇÕES DEVEM SER ATENDIDAS
+  const upload = () => {
     if (!nome) {
-      alert('Preencha o campo nome')
-      return
-    }
-    else if (!email) {
-      alert('Preencha o campo e-mail')
-      return
-    }
-    else if (!cpf) {
-      alert('Preencha o campo cpf')
+      Alert.alert('Preencha o campo nome')
       return
     }
     else if (!datanascimento) {
-      alert('Preencha o campo dataNascimento')
+      Alert.alert('Preencha o campo dataNascimento')
       return
+    }
+    else {
+      setPasso(2)
+    }
+  }
+  const upload2 = () => {
+    if (!cpf) {
+      Alert.alert('Preencha o campo cpf')
+    }
+    else {
+      setPasso(3)
+    }
+  }
+  const upload3 = () => {
+    if (!email) {
+      Alert.alert('Preencha o campo e-mail')
     }
     else if (!telefone) {
-      alert('Preencha o campo celular')
-      return
-    
+      Alert.alert('Preencha o campo celular')
     }
-
-    // const storageRef = ref(
-    //   storage,
-    //   `images/${nome.replace(/ +/g, '') + '_' + image.name}`
-    // )
-    // const uploadTask = uploadBytesResumable(storageRef, file)
-
-    // uploadTask.on('state_changed', snapshot => {
-    //   const progress = Math.round(
-    //     (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-    //   )
-    //   setTimeout(() => {
-    //     setProgressoPercent(progress), 1000
-    //   })
-    // })
+    else {
+      setPasso(5)
+    }
   }
-  function Calculo(){
-    var data = datanascimento
+  const upload4 = () => {
+    if (!senha) {
+      Alert.alert('Preencha o campo senha')
+    }
+    else if(senha.length < 8){
+      Alert.alert('A senha precisa ser maior que 8 caracteres')
+    }
+    else if(confirmarSenha.length < 8){
+      Alert.alert('A senha precisa ser maior que 8 caracteres')
+      
+    }
+    else if(confirmarSenha != senha){
+      Alert.alert('As senhas não conferem')
+      
+    }
     
   }
+
+
+  // const storageRef = ref(
+  //   storage,
+  //   `images/${nome.replace(/ +/g, '') + '_' + image.name}`
+  // )
+  // const uploadTask = uploadBytesResumable(storageRef, file)
+
+  // uploadTask.on('state_changed', snapshot => {
+  //   const progress = Math.round(
+  //     (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+  //   )
+  //   setTimeout(() => {
+  //     setProgressoPercent(progress), 1000
+  //   })
+  // })
+
+  // function Calculo(){
+  //   var data = datanascimento
+
+  // }
 
 
   // const criarConta = (nome, email, cpf, datanascimento, telefone) =>{
@@ -82,34 +114,71 @@ export default function Cadastro({ navigation }) {
               </Text>
             </View>
             <View className="flex w-[100%] items-center">
-              <TextInput className="w-[80%] mb-12 h-10 bg-slate-50 rounded-lg" placeholder="Digite o seu nome" keyboardType="default" onChange={(nome) => setNome(nome)} />
-              <TextInput className="w-[80%] mb-16 h-10 bg-slate-50 rounded-lg" placeholder="Digite a sua data de nascimento" keyboardType="phone-pad" onChange={(datanascimento) => setDatanascimento(datanascimento)} />
-              <Botao evento={() => setPasso(2)} />
+              <TextInput className="w-[80%] mb-12 h-10 bg-slate-50 rounded-lg" placeholder="Digite o seu nome" keyboardType="default" onChangeText={(e) => setNome(e)} />
+              <TextInput className="w-[80%] mb-16 h-10 bg-slate-50 rounded-lg" placeholder="Digite a sua data de nascimento" keyboardType="phone-pad" onChangeText={(e) => setDatanascimento(e)} />
+              <Botao evento={() => upload()} />
             </View>
           </View>
         </View>
         :
         passo == 2 ?
-        <View className="w-screen h-screen bg-black">
-        <View className="w-full pt-24 flex-1 items-center">
-          <View className="flex text-center items-center justify-center w-[90%]">
-            <Text className="text-[24px] text-[#5a5d68] pb-12">
-              Agora precisamos do seu CPF
-            </Text>
-            <Text className="text-[15px] pb-12 text-[#5a5d68] text-justify">
-              Suas informações estão seguras conosco!
-            </Text>
+          <View className="w-screen h-screen bg-black">
+            <View className="w-full pt-24 flex-1 items-center">
+              <View className="flex text-center items-center justify-center w-[90%]">
+                <Text className="text-[24px] text-[#5a5d68] pb-12">
+                  Agora precisamos do seu CPF
+                </Text>
+                <Text className="text-[15px] pb-12 text-[#5a5d68] text-justify">
+                  Suas informações estão seguras conosco!
+                </Text>
+              </View>
+              <View className="flex w-[100%] items-center">
+                <TextInput className="w-[80%] mb-16 h-10 bg-slate-50 rounded-lg" placeholder="Digite o seu CPF" maxLength={11} keyboardType="phone-pad" onChangeText={(e) => setCpf(e)} />
+                <Botao evento={() => upload2()} />
+              </View>
+            </View>
           </View>
-          <View className="flex w-[100%] items-center">
-          <TextInput className="w-[80%] mb-16 h-10 bg-slate-50 rounded-lg" placeholder="Digite o seu CPF" maxLength={11} keyboardType="phone-pad" onChange={(cpf) => setCpf(cpf)} />
-            <Botao evento={() => setPasso(2)} />
-          </View>
-        </View>
-      </View>
           :
-          null
+          passo == 3 ?
+            <View className="w-screen h-screen bg-black">
+              <View className="w-full pt-24 flex-1 items-center">
+                <View className="flex text-center items-center justify-center w-[90%]">
+                  <Text className="text-[24px] text-[#5a5d68] pb-12">
+                    Em qual telefone e e-mail podemos falar com você?
+                  </Text>
+                  <Text className="text-[15px] pb-12 text-[#5a5d68] text-justify">
+                    Será utilizado para informar sobre suas transações e novidades do nosso banco.
+                  </Text>
+                </View>
+                <View className="flex w-[100%] items-center">
+                  <TextInput className="w-[80%] mb-16 h-10 bg-slate-50 rounded-lg" placeholder="Digite o seu E-mail" keyboardType="default" onChangeText={(e) => setEmail(e)} />
+                  <TextInput className="w-[80%] mb-16 h-10 bg-slate-50 rounded-lg" placeholder="(00) 00000-0000" keyboardType="phone-pad" onChangeText={(e) => setTelefone(e)} />
+                  <Botao evento={() => upload3()} />
+                </View>
+              </View>
+            </View>
+              :
+              passo == 5?
+              <View className="w-screen h-screen bg-black">
+                <View className="w-full pt-24 flex-1 items-center">
+                  <View className="flex text-center items-center justify-center w-[90%]">
+                    <Text className="text-[24px] text-[#5a5d68] pb-12">
+                      Estamos quase lá! Crie a sua senha.
+                    </Text>
+                    <Text className="text-[15px] pb-12 text-[#5a5d68] text-justify">
+                      Lembrando que a senha precisa conter 8 números e números sortidos.
+                    </Text>
+                  </View>
+                  <View className="flex w-[100%] items-center">
+                    <TextInput className="w-[80%] mb-16 h-10 bg-slate-50 rounded-lg" placeholder="Digite a sua senha" keyboardType="phone-pad" onChangeText ={(e) => setSenha(e)} />
+                    <TextInput className="w-[80%] mb-16 h-10 bg-slate-50 rounded-lg" placeholder="confirmar senha" keyboardType="phone-pad" onChangeText={(e) => setConfirmarSenha(e)} />
+                    <Botao evento={() => upload4()} />
+                  </View>
+                </View>
+              </View>
+              :
+              null
       }
-
     </>
 
   )
